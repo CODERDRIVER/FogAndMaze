c5b9b26eb0c1ca3a6a8c3e9b63d2fe154cd83fa3
CODERDRIVER <1187697635@qq.com>
2018-09-09 00:19:53 +0800

    add index

---
+++ src/main/java/cn/xuyangl/fogandhaze/controller/IndexController.java
@@ -0,0 +1,24 @@
+package cn.xuyangl.fogandhaze.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description
+ * @Author: liuXuyang
+ * @studentNo 15130110024
+ * @Emailaddress 1187697635@qq.com
+ * @Date: 2018/9/9 00:08
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String home()
+    {
+        return "index";
+    }
+}
